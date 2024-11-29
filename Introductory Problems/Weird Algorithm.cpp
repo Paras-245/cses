7408a4fb496e04a7b45a4b596eeff73672c07f02
Paras-245 <paraschawla277@gmail.com>
2024-11-29 22:45:06 +0530

    weired algo + missing num

---
+++ Introductory Problems/Weird Algorithm.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define f(i,s,e) for(long long int i=s;i<e;i++)
+ 
+int main()
+{
+    long long n = 0;
+    cin >> n;
+    while(n != 1)
+    {
+        cout << n << " ";
+        if((n &1) ==0 )
+        {
+            n = n/2;
+        }
+        else
+        {
+            n = n*3 +1;
+        }
+    }
+    cout << 1 << endl;
+    return 0;
